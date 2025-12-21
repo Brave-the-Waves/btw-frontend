d042a9d87e93864af9c6ab85ee632b02910d3f4a
DorySong <dorysongmtl@gmail.com>
2025-12-21 11:54:59 -0500

    new pages for app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Success from './pages/Success'
 import Cancel from './pages/Cancel'
+import Teams from './pages/Teams'
+import TeamDetails from './pages/TeamDetails'
+import Leaderboard from './pages/Leaderboard'
+import Profile from './pages/Profile'
+import Participants from './pages/Participants'
 
 export default function App() {
   return (
@@ -11,6 +16,11 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/success" element={<Success />} />
         <Route path="/cancel" element={<Cancel />} />
+        <Route path="/teams" element={<Teams />} />
+        <Route path="/teams/:name" element={<TeamDetails />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/participants" element={<Participants />} />
       </Routes>
     </Router>
   )
